1c63ecc640ca1c59be58cac24172a60949542b72
expoli <me@expoli.tech>
2021-06-27 19:39:36 +0800

    修改脚本文件名称

--- start-bolo.sh
+++ start-bolo.sh
@@ -239,7 +239,7 @@ Check_pid_server() {
 Update_Shell() {
 	sh_new_ver=$(wget --no-check-certificate -qO- -t1 -T3 "https://raw.githubusercontent.com/expoli/start-bolo/master/start-bolo.sh"|grep 'SH_VERSION="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1) && sh_new_type="github"
 	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 无法链接到 Github !" && exit 0
-	wget -N --no-check-certificate "https://raw.githubusercontent.com/expoli/start-bolo/master/start-bolo.sh" && chmod +x install_docker.sh
+	wget -N --no-check-certificate "https://raw.githubusercontent.com/expoli/start-bolo/master/start-bolo.sh" && chmod +x start-bolo.sh
 	echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !(注意：因为更新方式为直接覆盖当前运行的脚本，所以可能下面会提示一些报错，无视即可)" && exit 0
 }
 # 检测部署目录
